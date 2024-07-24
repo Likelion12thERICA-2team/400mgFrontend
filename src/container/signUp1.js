eb3176f7ae61d5c2c2f577121d91f674b66a3524
kkeunii <greenlee55@naver.com>
2024-07-24 21:54:06 +0900

    signup1 페이지 제작

---
+++ src/container/signUp1.js
@@ -0,0 +1,16 @@
+
+import HowMuch from "../components/howMuch";
+
+const SignUp1 = () => {
+  return ( 
+    <div className="">
+      <main className=''>
+        <div className=''>
+          <HowMuch />
+        </div>
+      </main>
+    </div>
+  );
+}
+ 
+export default SignUp1;
