e64bb0ba616ef1dfec303412aad37ac1c1c4f080
Kyungjin <gulmok1217@hanyang.ac.kr>
2024-08-01 17:30:26 +0900

    [feat] 아이디, 비밀번호 긴급 구현

--- src/components/setNickname.js
+++ src/components/setNickname.js
@@ -12,7 +12,20 @@ const SetNickname = () => {
             </div>
 
             <div className="text-[16px] text-[#5A5A5A] font-[AppleMedium] mt-[125px] ml-[119px]">
-                당신의 이름은...
+                당신의 아이디...
+            </div>
+
+            <input 
+                type="text" 
+                className="w-[267px] h-[44px] font-AppleMedium text-center ml-[34px] mt-[49px] text-[22px]"    
+            />
+            <div 
+                className="w-[267px] my-[1%] border-[1px] border-[#999999] mb-[0%] mt-[0%] ml-[34px]">
+
+            </div>
+
+            <div className="text-[16px] text-[#5A5A5A] font-[AppleMedium] mt-[125px] ml-[119px]">
+                당신의 비밀번호...
             </div>
 
             <input 
