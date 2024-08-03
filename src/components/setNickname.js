c4431351d23f28a8439fc1139cf4d0f795fff5a3
bamcasa <combeesang@gmail.com>
2024-08-03 09:00:44 +0900

    [chore]: 체리픽
    seNickName.js commit해서
    로그인/회원가입 페이지 반영

--- src/components/setNickname.js
+++ src/components/setNickname.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import Button from "./Button";
-import axios from "axios";
-
-// const axios = require("axios");
 import axios from "axios";
 
 // const axios = require("axios");
